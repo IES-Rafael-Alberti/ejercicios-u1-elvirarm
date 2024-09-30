ab5ebb7d29a835db08cc2677d92f751b7d6d2d89
elvirarm <rmelvira@iesrafaelalberti.es>
2024-09-30 20:02:02 +0200

    Ejercicio 1-4

--- src/ej04.py
+++ src/ej04.py
@@ -3,6 +3,6 @@
 # Escribe un programa que le pida al usuario una temperatura en grados Celsius, la convierta a grados Fahrenheit e imprima por pantalla la temperatura convertida.
 #
 
-tempcelsius = int(input("Dame una temperatura en grados Celsius: "))
-tempfahrenheit = ((tempcelsius * 9/5)+32)
-print (f"La temperatura convertida es: {tempfahrenheit}")
+tempCelsius =  float(input("Dame una temperatura en grados Celsius:"))
+tempFahrenheit = ((tempCelsius * 9/5)+32)
+print (f"La temperatura en grados Farenheit es: {tempFahrenheit}ºF ({tempCelsius}ºC)")
