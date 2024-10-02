c4b2c612e66a117e754a37d91a1d6e20f9de1a30
elvirarm <rmelvira@iesrafaelalberti.es>
2024-10-02 17:36:25 +0200

    Ej 1-15 + 16 sin terminar

---
+++ src/ej16.py
@@ -0,0 +1,10 @@
+#
+#Ejercicio 1.2.16¶
+#Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60%. Escribir un programa que comience leyendo el número de barras vendidas que no son del día. Después el programa debe mostrar el precio habitual de una barra de pan (establecido en el programa como una constante), el descuento que se le hace por no ser fresca y el coste final total de todas las barras no frescas.
+#
+ 
+PRECIO_PAN = 3.49
+num_pan_antiguo = int(input("¿Cuántas barras de pan que no son del día se han vendido?: "))
+
+
+
