17c65f2b2baa84955a33b8490726c7c551032e3e
elvirarm <rmelvira@iesrafaelalberti.es>
2024-10-08 10:35:21 +0200

    1-26 completos

--- src/ej24.py
+++ src/ej24.py
@@ -8,5 +8,11 @@ precio_euros = int(partes[0])
 precio_centimos = (partes[1])
 if len(precio_centimos) == 2:
     print (f"El precio es de {precio_euros} euros y {precio_centimos} céntimos")
-else:
-    input ("**ERROR** Los céntimos tienen que tener dos decimales, inténtalo de nuevo:")
+while len(precio_centimos) != 2:
+    error = input ("**ERROR** Los céntimos tienen que tener dos decimales, inténtalo de nuevo:")
+    partes_error = error.split(".")
+    error_euros = int(partes_error[0])
+    error_decimales = (partes_error[1])
+    if len(error_decimales) == 2:
+        print (f"El precio es de {error_euros} euros y {error_decimales} céntimos")
+        break
